4e222b2338d9a134ebf452ac10fc38dc579017f3
Ajitesh Abhishek <ajiteshleo@gmail.com>
2019-05-05 09:22:20 -0500

    second edit

--- peak1.py
+++ peak1.py
@@ -1,19 +1,31 @@
-A=[1,1,1,2,4,5,6,7,8,9,0]
+A=[11,12,13,14,11]
 
 def peakfinder(Array):
-    mid=len(Array)//2
+    mid=(len(Array)-1)//2
     print(f"{Array[mid]}")
-    if Array[mid]<=Array[mid-1]:
-        return peakfinder(Array[:mid])
-    elif Array[mid]<=Array[mid+1]:
-        return peakfinder(Array[mid:])
-    else:
+
+    #deal with the case when there is just one element and it's the peak
+    if len(Array)==1:
         return Array[mid]
+
+    # Deal with the case when two elements and edge element is the peak
+    elif len(Array)==2:
+        return max(Array[0], Array[1])
+
+    else:
+        if Array[mid]<=Array[mid-1]:
+            print(Array[:mid])
+            return peakfinder(Array[:mid])
+        elif Array[mid]<=Array[mid+1]:
+            print(Array[mid:])
+            return peakfinder(Array[mid:])
+        else:
+            return Array[mid]
 
 def main():
     peak=peakfinder(A)
     print(f"Element order")
-    print("-------------")
+    print("-----------------")
     print(f"One peak in the array is {peak}")
 
 
